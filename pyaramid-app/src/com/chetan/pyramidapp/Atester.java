fd4b3c7c96dd287c44c0ddd1984a291699f6a997
chetanmelavanki <chetanmelavanki2000@gmail.com>
2022-03-15 08:15:14 +0530

    add pyramid

---
+++ pyaramid-app/src/com/chetan/pyramidapp/Atester.java
@@ -0,0 +1,19 @@
+package com.chetan.pyramidapp;
+
+public class Atester {
+	public static void main(String[] args) {
+
+        String input = "Geeks for Geeks";
+ 
+        StringBuilder input1 = new StringBuilder();
+ 
+        // append a string into StringBuilder input1
+        input1.append(input);
+ 
+        // reverse StringBuilder input1
+        input1.reverse();
+ 
+        // print reversed String
+        System.out.println(input1);
+    }
+}
